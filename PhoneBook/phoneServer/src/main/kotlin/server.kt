3e5a3de9c3a5159a08c930030935617d3cfa8093
abondar24 <abondar1992@gmail.com>
2021-01-29 00:36:36 +0100

    pom clean up

--- PhoneBook/phoneServer/src/main/kotlin/server.kt
+++ PhoneBook/phoneServer/src/main/kotlin/server.kt
@@ -5,24 +5,14 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.html.*
 
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor"
-        }
-    }
-}
+
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         routing {
             get("/") {
-                call.respondHtml(HttpStatusCode.OK, HTML::index)
+
             }
         }
     }.start(wait = true)
