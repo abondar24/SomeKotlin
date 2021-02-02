99e2c9c20808e7c97b31b51bca3214f30aba4e71
abondar24 <abondar1992@gmail.com>
2021-02-02 11:25:40 +0100

    dbconf update

--- PhoneBook/phoneServer/src/main/kotlin/org/abondar/experimental/phone/server/conf/DbConfig.kt
+++ PhoneBook/phoneServer/src/main/kotlin/org/abondar/experimental/phone/server/conf/DbConfig.kt
@@ -1,8 +1,10 @@
 package org.abondar.experimental.phone.server.conf
 
+import com.typesafe.config.ConfigFactory
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
+import io.ktor.config.*
 import io.ktor.util.KtorExperimentalAPI
 import org.abondar.experimental.phone.server.model.Contacts
 import org.abondar.experimental.phone.server.model.Users
@@ -12,18 +14,33 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 
 
-const val HIKARI_CONFIG_KEY = "ktor.hikariconfig"
 
 @KtorExperimentalAPI
 fun Application.initDB(){
-    val config = environment.config
-        .property(HIKARI_CONFIG_KEY).getString()
+    val conf = HoconApplicationConfig(ConfigFactory.load())
 
-    val dbConfig = HikariConfig(config)
-    val dataSource = HikariDataSource(dbConfig)
+    val dataSource = hikary(conf)
     Database.connect(dataSource)
     createTables()
     LoggerFactory.getLogger(Application::class.simpleName).info("Initialized Database")
+}
+
+
+private fun hikary(conf : HoconApplicationConfig) : HikariDataSource{
+    val dbConf = HikariConfig()
+
+    dbConf.driverClassName = conf.property("dataSource.className").getString()
+    dbConf.username=conf.property("dataSource.user").getString()
+    dbConf.password=conf.property("dataSource.password").getString()
+    dbConf.jdbcUrl=conf.property("dataSource.url").getString()
+    dbConf.maximumPoolSize=conf.property("dataSource.maxPoolSize").getString().toInt()
+    dbConf.isAutoCommit=conf.property("dataSource.autoCommit").getString().toBoolean()
+    dbConf.transactionIsolation=conf.property("dataSource.transactionIsolation").getString()
+
+
+    dbConf.validate()
+
+    return HikariDataSource(dbConf)
 }
 
 private fun createTables() = transaction {
