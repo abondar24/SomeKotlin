2908fc9563036153d2a25946d9e2b9ede1a5b807
abondar24 <abondar1992@gmail.com>
2021-02-06 20:52:27 +0100

    service inject

--- PhoneBook/phoneServer/src/main/kotlin/org/abondar/experimental/phone/server/service/ServiceInject.kt
+++ PhoneBook/phoneServer/src/main/kotlin/org/abondar/experimental/phone/server/service/ServiceInject.kt
@@ -1,11 +1,14 @@
 package org.abondar.experimental.phone.server.service
 
+import org.abondar.experimental.phone.server.org.abondar.experimental.phone.server.service.LoginService
 import org.abondar.experimental.phone.server.org.abondar.experimental.phone.server.service.UserService
 import org.kodein.di.DI
 import org.kodein.di.bind
+import org.kodein.di.instance
 import org.kodein.di.singleton
 
 val serviceDI = DI{
      bind<UserService>() with singleton { UserService() }
+     bind<LoginService>() with singleton { LoginService(instance(UserService())) }
 }
 
